ccf7b7c2f0bb406fb917de54a2cb4e1cc21e41f8
Maksym Baboshko <maksym.baboshko@yahoo.com>
2020-09-22 09:16:25 +0300

    Added transition when clicking on the logo

--- client/src/components/NotesSide.js
+++ client/src/components/NotesSide.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { changeActive } from '../store/actions'
 import NotesFooter from './NotesFooter'
 import NotesList from './NotesList'
 import Search from './Search'
 
 export default function NotesSide() {
+  const { active } = useSelector(state => state.notes)
+  const dispatch = useDispatch()
+
+  const onLogoHandler = () => {
+    if (active) dispatch(changeActive(null))
+  }
+
   return (
     <div className="notes__side">
       <header className="notes__header">
-        <h1 className="notes__logo">Notes</h1>
+        <Link to="/" className="notes__logo" onClick={onLogoHandler}>
+          Notes
+        </Link>
       </header>
 
       <Search />
